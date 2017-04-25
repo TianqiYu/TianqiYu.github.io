ba1a15396f55b32ee55fbe4878f242fd07b1209f
Tianqi Yu <yutianqijy@gmail.com>
2017-04-25 17:27:55 -0400

    Update post.js

--- js/post.js
+++ js/post.js
@@ -1,9 +1,9 @@
 $(function() {
     $('pre').addClass('prettyprint linenums'); //添加Google code Hight需要的class
 
-    window.disqus_shortname = 'beiyuu'; // required: replace example with your forum shortname
+    window.disqus_shortname = 'Tianqi'; // required: replace example with your forum shortname
     $('#disqus_container .comment').on('click',function(){
-        $(this).html('加载中...');
+        $(this).html('Loading(加载中)...');
         var that = this;
         $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
     });
